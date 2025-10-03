c17bd89b1b244b1bb4a0dff479b3152572a5a10d
Rafa6235 <rafadyaksa@students.amikom.ac.id>
2025-10-03 19:39:37 +0700

    Add files via upload

---
+++ CalculatorConsoleApp2/CalculatorConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Aplikasi Calculator Versi Console";
+
+            Console.Write("Inputkan nilai a = ");
+            int a = int.Parse(Console.ReadLine()); // Casting
+
+            Console.Write("Inputkan nilai b = ");
+            var b = Convert.ToInt32(Console.ReadLine()); // Casting
+
+            Console.WriteLine(); // Tambahkan baris kosong
+            Console.WriteLine("Hasl Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
+            Console.WriteLine("Hasl Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+            Console.WriteLine("Hasl Perkalian: {0} x {1} = {2}", a, b, Perkalian(a, b));
+            Console.WriteLine("Hasl Pembagian: {0} / {1} = {2}", a, b, Pembagian(a, b));
+
+            Console.ReadKey();
+        }
+
+        static int Penambahan(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+
+        static int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+
+        static int Pembagian(int a, int b)
+        {
+            return a / b;
+        }
+
+    }
+}
---
+++ CalculatorConsoleApp2/CalculatorWindowsFormsApp/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorWindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int Penambahan(int a, int b)
+        {
+            return a + b;
+        }
+
+        private int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+
+        private int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+
+        private int Pembagian(int a, int b)
+        {
+            return a / b;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int a = int.Parse(textBox1.Text);
+                int b = int.Parse(textBox2.Text);
+                int hasil = 0;
+
+                switch (comboBox1.SelectedItem.ToString())
+                {
+                    case "Penambahan":
+                        hasil = Penambahan(a, b);
+                        break;
+                    case "Pengurangan":
+                        hasil = Pengurangan(a, b);
+                        break;
+                    case "Perkalian":
+                        hasil = Perkalian(a, b);
+                        break;
+                    case "Pembagian":
+                        if (b == 0)
+                        {
+                            MessageBox.Show("Tidak bisa membagi dengan nol!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+                        hasil = Pembagian(a, b);
+                        break;
+                    default:
+                        MessageBox.Show("Pilih operasi terlebih dahulu.", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                }
+
+                textBox3.Text = hasil.ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Input harus berupa angka!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
